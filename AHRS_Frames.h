8377a9801dfea2592391a91192d5fea121e60fd4
Piotr Zieliński <piter.t.ziel@gmail.com>
2020-01-14 02:57:00 +0100

    MID enums and message struct
    - Added enums containing MID hex values.
    - Added message structure

--- AHRS_Frames.h
+++ AHRS_Frames.h
@@ -11,10 +11,10 @@
 #include <stdint.h> //Just for Visual Studio Code's InteliJ
 
 /* Frames lengths - begin*/
-#define STD_MSG_MAX_PAYLOAD_LEN   254                      //Max length of payload in standard frame
-#define STD_MSG_SIZEMAX           STD_MSG_MAX_PAYLOAD_LEN + 5    //Max length of whole standard frame
-#define EXT_MSG_MAX_PAYLOAD_LEN   2048                     //Max length of payload in extended frame
-#define EXT_MSG_SIZEMAX           EXT_MSG_MAX_PAYLOAD_LEN + 8    //Max length of whole extended frame
+#define STD_MSG_MAX_PAYLOAD_LEN 254                 //Max length of payload in standard frame
+#define STD_MSG_SIZEMAX STD_MSG_MAX_PAYLOAD_LEN + 5 //Max length of whole standard frame
+#define EXT_MSG_MAX_PAYLOAD_LEN 2048                //Max length of payload in extended frame
+#define EXT_MSG_SIZEMAX EXT_MSG_MAX_PAYLOAD_LEN + 8 //Max length of whole extended frame
 /* Frames lengths - end*/
 
 typedef uint8_t AHRS_RawStdFrame[STD_MSG_SIZEMAX];
@@ -24,71 +24,99 @@ typedef uint8_t AHRS_RawExtFrame[EXT_MSG_SIZEMAX];
 typedef struct
 {
     double q[4];
-}Pocket_Quaternions;
+} Pocket_Quaternions;
 
 typedef struct
 {
     double Roll, Pitch, Yaw;
-}Pocket_EulerAngles;
+} Pocket_EulerAngles;
 
 typedef struct
 {
-    double a,b,c,d,e,f,g,h,i;
-}Pocket_RotationMatrix;
+    double a, b, c, d, e, f, g, h, i;
+} Pocket_RotationMatrix;
 
 typedef struct
 {
     double dv_x, dv_y, dv_z;
-}Pocket_DeltaV;
+} Pocket_DeltaV;
 
 typedef struct
 {
     double dq[4];
-}Pocket_DeltaQ;
+} Pocket_DeltaQ;
 
 typedef struct
 {
     double accX, accY, accZ;
-}Pocket_Acceleration;
+} Pocket_Acceleration;
 
 typedef struct
 {
     double freeAccX, freeAccY, freeAccZ;
-}Pocket_FreeAcceleration;
+} Pocket_FreeAcceleration;
 
 typedef struct
 {
     double accX, accY, accZ;
-}Pocket_AccelerationHR;
+} Pocket_AccelerationHR;
 
 typedef struct
 {
     double gyrX, gyrY, gyrZ;
-}Pocket_RateOFTurn;
+} Pocket_RateOFTurn;
 
 typedef struct
 {
     double gyrX, gyrY, gyrZ;
-}Pocket_RateOFTurnHR;
+} Pocket_RateOFTurnHR;
+
+typedef struct
+{
+    double gyrX, gyrY, gyrZ;
+} Pocket_MagneticField;
 /*Structures defining possible pockets - end*/
 
-typedef struct 
+/*Enum containing possible pocket types and hex values of MID - begin*/
+typedef enum
 {
-    typedef enum PocketMID_t
-    {
-        quaternion, freeacc
-    } PocketMID;
+    QuaternionsENU = 0x2010,
+    EulerAnglesENU = 0x2030,
+    RotationMatrixENU = 0x2020,
+    QuaternionsNED = 0x2014,
+    EulerAnglesNED = 0x2034,
+    RotationMatrixNED = 0x2024,
+    DeltaV = 0x4010,
+    DeltaQ = 0x8030,
+    Acceleration = 0x4020,
+    FreeAcceleration = 0x4030,
+    AccelerationHR = 0x4040,
+    RateOFTurn = 0x8020,
+    RateOFTurnHR = 0x8040,
+    MagneticField = 0xC020,
+} PocketMID_t;
+/*Enum containing possible pocket types and hex values of MID - begin*/
 
-    union PocketData
-    {
+/*Union containing all possible pockets types - begin*/
+typedef union {
+    Pocket_Quaternions Quaternions;
+    Pocket_EulerAngles EulerAngles;
+    Pocket_RotationMatrix RotationMatrix;
+    Pocket_DeltaV DeltaV;
+    Pocket_DeltaQ DeltaQ;
+    Pocket_Acceleration Acceleration;
+    Pocket_FreeAcceleration FreeAcceleration;
+    Pocket_AccelerationHR AccelerationHR;
+    Pocket_RateOFTurn RateOFTurn;
+    Pocket_RateOFTurnHR RateOFTurnHR;
+    Pocket_MagneticField MagneticField;
+} PocketData_t;
+/*Union containing all possible pockets types - begin*/
 
-    }
-
-    Po
-
-
-}Pocket_t;
-
-
+typedef struct
+{
+    PocketData_t Payload;
+    PocketMID_t MID;
+} AHRS_Message;
 
 #endif /* AHRS_FRAMES_H_ */
